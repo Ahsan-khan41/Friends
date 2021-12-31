49aaf4761c0f2a3f4541573c5957403c422b4069
Muhammad Ahsan Khan <makford41@gmail.com>
2021-12-31 17:02:39 +0500

    posts updated

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Card, Avatar } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { PostForm } from "./PostForm";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, doc, onSnapshot } from "firebase/firestore";
 import { fireDB } from '../../firebaseConfig'
 
 function Posts() {
@@ -10,52 +10,61 @@ function Posts() {
     const { Meta } = Card;
     const [postsArr, setPostsArr] = useState([]);
 
+    const dataHandler = (document, arr) => {
+        onSnapshot(doc(fireDB, "users", `${document}`), (doc) => {
+            //console.log("Current data: " , doc.data());
+            arr.push(doc.data());
+            //console.log(arr);
+            setPostsArr(arr);
+        });
+    }
+
     useEffect(() => {
 
         onSnapshot(collection(fireDB, "posts"), (doc) => {
             let arr = [];
-            doc.forEach((element) => {
-                arr.push(element.data());
+            doc.forEach((elem) => {
+                // console.log(elem.data().postedBy);
+                dataHandler(elem.data().postedBy, arr);
+                arr.push(elem.data());
             });
-            setPostsArr(arr);
         });
-        
+
     }, [])
 
-    // console.log(postsArr);
+    console.log(postsArr);
 
 
     return (
         <>
-
             <div>
                 <PostForm />
                 <div id='posts' >
                     {postsArr.map((elem, index) => {
-                        return(
-                            <Card key = {index}
-                        style={{ width: "50%", margin: 'auto', marginBottom: 20, borderRadius: '20px' }}
-                        cover={
-                            <img
-                                alt="example"
-                                src={elem.url}
-                            />
-                        }
-                        actions={[
-                            <SettingOutlined key="setting" />,
-                            <EditOutlined key="edit" />,
-                            <EllipsisOutlined key="ellipsis" />,
-                        ]}
-                    >
-                        <Meta
-                            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                            title={elem.postTitle}
-                            description={elem.description}
-                        />
-                    </Card>
+                        return (
+                            <Card key={index}
+                                style={{ width: "50%", margin: 'auto', marginBottom: 20, borderRadius: '20px' }}
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src={elem.url}
+                                    />
+                                }
+                                actions={[
+                                    <SettingOutlined key="setting" />,
+                                    <EditOutlined key="edit" />,
+                                    <EllipsisOutlined key="ellipsis" />,
+                                ]}
+                            >
+                                <Meta
+                                    avatar={<Avatar src={elem.profileUrl} />}
+                                    title={elem.postedBy}
+                                    description={elem.description}
+                                />
+                            </Card>
                         )
                     })}
-                    
+
                 </div>
 
             </div>
